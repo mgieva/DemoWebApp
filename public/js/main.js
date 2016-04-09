7bf6f23bc100984987ff95e2b763dd51d7efc436
Magdalena Georgieva <mgeorgieva@hubspot.com>
2016-04-09 14:02:16 -0400

    adding a blog post

--- public/js/main.js
+++ public/js/main.js
@@ -1,5 +1,12 @@
+var counter = 0;
+var ids = ["leaders", "prep-work", "agenda", "supporters", "documentation"]
+
 
 function myFunction() {
-  document.getElementById("demo").innerHTML = "You get some ice cream!";
+  document.getElementById(ids[counter]).style.display = "block";
+  counter = counter + 1
+  if (ids.length == counter) {
+  	document.getElementById("read-more").style.display = "none";
+  }
 }
 
